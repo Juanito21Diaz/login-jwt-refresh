8de5d9ced7beb723048fbc9976b9fe093e6adf1b
Juan Esteban JEDJ. Diaz Jaramillo <JDiazJ@COONFIECOOP.LOCAL>
2026-01-19 11:41:02 -0500

    Actualización: Rutas

--- Practica-Express/app.js
+++ Practica-Express/app.js
@@ -2,8 +2,22 @@ const express = require('express');
 const app = express();
 
 const {infoCursos} = require('./cursos');
+
+//rutas
 app.get('/', (req, res) => {
     res.send("Servidor Express en marcha");
+});
+
+app.get('/api/cursos', (req, res) => {
+    res.send(JSON.stringify(infoCursos));
+});
+
+app.get('/api/cursos/programacion', (req, res) => {
+    res.send(JSON.stringify(infoCursos.programacion));
+});
+
+app.get('/api/cursos/literatura', (req, res) => {
+    res.send(JSON.stringify(infoCursos.literatura));
 });
 
 const PUERTO = process.env.PORT || 3000;
